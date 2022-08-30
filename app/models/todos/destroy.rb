067d806b5629e833874e55b269298f2b739c0b16
nicolaskelepuris <nicolaskelepuris@gmail.com>
2022-08-30 10:32:12 -0300

    add module Todos to use cases

--- app/models/todos/destroy.rb
+++ app/models/todos/destroy.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-class Todos::Destroy < Micro::Case
-  attributes :user_id, :id
+module Todos
+  class Destroy < Micro::Case
+    attributes :user_id, :id
 
-  def call!
-    todo = Todo.find_by(id: id, user_id: user_id)
+    def call!
+      todo = Todo.find_by(id: id, user_id: user_id)
 
-    return Failure :not_found, result: { todo: { id: 'not found' } } if todo.nil?
+      return Failure :not_found, result: { todo: { id: 'not found' } } if todo.nil?
 
-    todo.destroy
+      todo.destroy
 
-    Success result: { todo: todo.serialize_as_json }
+      Success result: { todo: todo.serialize_as_json }
+    end
   end
 end
--- app/models/todos/update.rb
+++ app/models/todos/update.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
-class Todos::Update < Micro::Case
-  attributes :user_id, :id, :title, :due_at
+module Todos
+  class Update < Micro::Case
+    attributes :user_id, :id, :title, :due_at
 
-  def call!
-    todo = Todo.find_by(id: id, user_id: user_id)
+    def call!
+      todo = Todo.find_by(id: id, user_id: user_id)
 
-    return Failure :not_found, result: { todo: { id: 'not found' } } if todo.nil?
+      return Failure :not_found, result: { todo: { id: 'not found' } } if todo.nil?
 
-    todo.update(title: title, due_at: due_at)
+      todo.update(title: title, due_at: due_at)
 
-    return Failure :unprocessable_entity, result: { todo: todo.errors.messages } unless todo.valid?
+      return Failure :unprocessable_entity, result: { todo: todo.errors.messages } unless todo.valid?
 
-    Success result: { todo: todo.serialize_as_json }
+      Success result: { todo: todo.serialize_as_json }
+    end
   end
 end
