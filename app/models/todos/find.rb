a437dd2e1eb0ad194bfe2c9154670f1a5515b64f
nicolaskelepuris <nicolaskelepuris@gmail.com>
2022-08-30 10:19:30 -0300

    add Todos::Find use case

---
+++ app/models/todos/find.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Todos::Find < Micro::Case
+  attributes :user_id, :id
+
+  def call!
+    todo = Todo.find_by(id: id, user_id: user_id)
+
+    return Failure :not_found, result: { todo: { id: 'not found' } } if todo.nil?
+
+    Success result: { todo: todo.serialize_as_json }
+  end
+end
