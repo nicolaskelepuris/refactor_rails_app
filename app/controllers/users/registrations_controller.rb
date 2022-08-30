745d170435e39fb0bf1ce4967506e8690822486e
nicolaskelepuris <nicolaskelepuris@gmail.com>
2022-08-30 09:41:22 -0300

    refactor registrations controller to use Users::Create use case

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,36 +2,15 @@
 
 class Users::RegistrationsController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
-
-    password = user_params[:password].to_s.strip
-    password_confirmation = user_params[:password_confirmation].to_s.strip
-
-    errors = {}
-    errors[:password] = ["can't be blank"] if password.blank?
-    errors[:password_confirmation] = ["can't be blank"] if password_confirmation.blank?
-
-    if errors.present?
-      render_json(422, user: errors)
-    else
-      if password != password_confirmation
-        render_json(422, user: { password_confirmation: ["doesn't match password"] })
-      else
-        password_digest = Digest::SHA256.hexdigest(password)
-
-        user = User.new(
-          name: user_params[:name],
-          email: user_params[:email],
-          token: SecureRandom.uuid,
-          password_digest: password_digest
-        )
-
-        if user.save
-          render_json(201, user: user.as_json(only: [:id, :name, :token]))
-        else
-          render_json(422, user: user.errors.as_json)
-        end
-      end
+    ::Users::Create.call(user_params) do |on|
+      on.failure(:unprocessable_entity) { |result| render status: 422, json: { user: result[:user] } }
+      on.success(:user_created) { |result| render status: 201, json: { user: result[:user] } }
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation).to_h
+    end
 end
